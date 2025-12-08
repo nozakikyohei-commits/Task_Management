414382c9470c000f81a4ed9c5e4a5a3cb9959acd
nozaki1111 <nozaki.kyohei@alhinc.jp>
2025-12-08 11:24:32 +0900

    [add]一時保存

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,13 +1,17 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.demo.form.RegistUserForm;
 
 @Controller
 public class UserController {
 	
 	@GetMapping("/create-user")
-	private String view() {
+	private String view(Model model) {
+		model.addAttribute("form", new RegistUserForm());
 		return "create-user";
 	}
 
--- src/main/java/com/example/demo/form/RegistUserForm.java
+++ src/main/java/com/example/demo/form/RegistUserForm.java
@@ -1,5 +1,26 @@
 package com.example.demo.form;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class RegistUserForm {
+	
+	@NotBlank(message = "{E0004}")
+	@Size(max = 20, message = "{E0005}")
+	private String name;
+	
+	@NotBlank(message = "{E0001}")
+	@Pattern(regexp = "^[ -~]{,255}$", message = "{E0006}")
+	private String mailAddress;
+	
+	@NotBlank(message = "{E0002}")
+	private String password;
+	
+	
 
 }
