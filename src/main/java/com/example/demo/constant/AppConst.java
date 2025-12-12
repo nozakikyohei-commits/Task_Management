93b523913b06d50a6f8d727018e203106413e398
nozaki1111 <nozaki.kyohei@alhinc.jp>
2025-12-12 15:22:52 +0900

    [add]定数の追加

--- src/main/java/com/example/demo/constant/AppConst.java
+++ src/main/java/com/example/demo/constant/AppConst.java
@@ -36,6 +36,7 @@ public class AppConst {
     }
 	
 	public static class TaskImportance {
+		public static final int UNSET = 0;
         public static final int LOW = 1;
         public static final int MEDIUM = 2;
         public static final int HIGH = 3;
