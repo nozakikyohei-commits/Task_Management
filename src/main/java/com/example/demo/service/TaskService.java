f821832a64fd0315d71effdcd2c2188c9497b451
nozaki1111 <nozaki.kyohei@alhinc.jp>
2025-12-18 14:06:40 +0900

    [add]タスク追加処理

--- src/main/java/com/example/demo/service/TaskService.java
+++ src/main/java/com/example/demo/service/TaskService.java
@@ -1,10 +1,12 @@
 package com.example.demo.service;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.example.demo.constant.AppConst;
 import com.example.demo.entity.Task;
 import com.example.demo.form.CreateTaskForm;
 import com.example.demo.form.EditTaskForm;
@@ -65,8 +67,37 @@ public class TaskService {
 		task.setDeadline(form.getDeadline());
 		task.setCompletedDate(form.getCompletedDate());
 		
+		int newStatus = determineStatus(form.getCompletedDate(), form.getDeadline());
+		task.setStatus(newStatus);
+		
 		taskMapper.update(task);
 	}
+	
+		//タスク更新時に、フォームに入力されている完了日と期限に応じたステータスを返すメソッド
+		private int determineStatus(LocalDate completedDate, LocalDate deadline) {
+			
+			LocalDate today = LocalDate.now();
+	
+			//完了日が設定されている場合
+			if (completedDate != null) {
+				//期限が設定されていて、かつ「完了日」が「期限」を過ぎている場合
+				if (deadline != null && completedDate.isAfter(deadline)) {
+					return AppConst.TaskStatus.EXPIRED_COMPLETED; //期限切れ完了 
+				}
+				//それ以外は通常の「完了」
+				return AppConst.TaskStatus.COMPLETED; //完了
+			}
+			
+			//完了日が設定されていない場合
+			else {
+				//期限が設定されていて、かつ「期限」が「今日」より前の場合
+				if (deadline != null && deadline.isBefore(today)) {
+					return AppConst.TaskStatus.EXPIRED; //期限切れ
+				}
+				//それ以外は「未完了」
+				return AppConst.TaskStatus.INCOMPLETE; //未完了
+			}
+		}
 	
 	@Transactional(readOnly = false)
 	public void updateStatusToExpired(int userId) {
@@ -77,9 +108,15 @@ public class TaskService {
 	public void updateStatusToCompleted(int taskId, int userId) {
 		taskMapper.updateStatusToCompleted(taskId, userId);
 	}
+	
 	@Transactional(readOnly = false)
 	public void updateStatusToIncompleted(int taskId, int userId) {
 		taskMapper.updateStatusToIncompleted(taskId, userId);
+	}
+	
+	@Transactional(readOnly = false)
+	public void delete(int taskId, int userId) {
+		taskMapper.delete(taskId, userId);
 	}
 
 }
