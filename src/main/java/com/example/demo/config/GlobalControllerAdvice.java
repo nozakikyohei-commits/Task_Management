2cf6b772be717f232daf3225ff6f4c7666caeba8
nozaki1111 <nozaki.kyohei@alhinc.jp>
2025-12-19 16:35:02 +0900

    [add]すべてのコントローラーにログインユーザー情報を渡す処理を追加

--- src/main/java/com/example/demo/config/GlobalControllerAdvice.java
+++ src/main/java/com/example/demo/config/GlobalControllerAdvice.java
@@ -1,7 +1,11 @@
 package com.example.demo.config;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
+
+import com.example.demo.authentication.CustomUserDetails;
+import com.example.demo.entity.User;
 
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -13,5 +17,18 @@ public class GlobalControllerAdvice {
     @ModelAttribute("currentUri")
     public String getCurrentUri(HttpServletRequest request) {
         return request.getRequestURI();
+    }
+    
+    //全てのコントローラーに『model.addAttribute("user", userDetails.getUser());』を自動で追加
+    @ModelAttribute("user")
+    public User addCurrentUserToModel(@AuthenticationPrincipal CustomUserDetails userDetails) {
+        
+        //ログイン画面やユーザー登録画面など、まだログインしていない状態ではuserDetailsがnullになるため、nullを返す
+        if (userDetails == null) {
+            return null;
+        }
+        
+        //ログインしている場合はログインユーザーの情報を渡す
+        return userDetails.getUser();
     }
 }
